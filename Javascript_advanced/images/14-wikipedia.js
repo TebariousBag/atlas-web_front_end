8b8cee8dd810834b6726e2f54475bfac88937cfe
“Tristian <tristian.davis@atlasschool.com>
2025-03-31 20:50:43 -0500

    gonna stop here for the moment to look into this more

---
+++ Javascript_advanced/images/14-wikipedia.js
@@ -0,0 +1,10 @@
+function createElement(data) {
+  // create paragraph with data as the text
+  const pp = document.createElement("p");
+  pp.textContent = data;
+  document.body.appendChild(pp);
+}
+
+function queryWikipedia(callback) {
+
+}
